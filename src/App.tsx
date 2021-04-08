d5c705edba9044652bd87f43fae34c85ebec06d4
Vladimir Lebedev <lebedev.vladimir3@gmail.com>
2021-04-08 04:55:38 +0300

    add 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Editor from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+import Drawer from './components/drawer';
+import challenge1 from './challenges/1';
+import {
+  Link,
+  useParams,
+  useLocation,
+  Route
+} from "react-router-dom";
+import React, { useEffect } from 'react';
+
+const pages = Array.from(Array(10).keys())
+
+interface ParamTypes {
+  number: string
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Make variable type string</div>
+
+      <Drawer>
+        <div className="numbers">
+          {pages.map(number => <Link to={`/exercise/${number}`} key={`number-${number}`}>
+            <div className="numbers--item">{number}</div>
+          </Link>)}
+        </div>
+        <Route path={`/exercise/:number`}>
+          <Inside/>
+        </Route>
+      </Drawer>
+
     </div>
   );
 }
+
+function Inside() {
+  function usePageViews() {
+    let location = useLocation()
+
+    useEffect(
+      () => {
+        this.forceUpdate()
+      },
+      [location]
+    )
+  }
+
+  let {number = 1} = useParams<ParamTypes>();
+  const importString = require(`./challenges/${number}`).default
+  console.log(importString)
+  console.log(number)
+
+  function handleEditorValidation(markers: monaco.editor.IMarker[]) {
+    // model markers
+    markers.forEach(marker => {
+      console.log("onValidate:", marker.message)
+    });
+  }
+
+  function handleEditorChange(value: string | undefined) {
+    // console.log(value)
+  }
+  usePageViews()
+  return (<Editor
+    height="40vh"
+    defaultLanguage="typescript"
+    defaultValue={importString}
+    onChange={handleEditorChange}
+    onValidate={handleEditorValidation}
+  />)
+}
+
 
 export default App;
---
+++ src/challenges/1.tsx
@@ -0,0 +1,6 @@
+export default `
+type AppProps = {}; // add appropriate type
+
+// do not change this
+const App = ({ message }: AppProps) => message.toLowerCase();
+`
